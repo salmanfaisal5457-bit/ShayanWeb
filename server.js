068589d09d5aca39fbcf23e795c52dc3b63b22a0
JUTT PLAYS YT <salmanfaisal5457@gmail.com>
2026-07-07 15:50:38 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const limit = require("express-rate-limit");
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/downloads", limit({
+    windowMs: 1 * 60 * 1000,
+    max: 20,
+    message: "<h1>429</h1>Too many requests."
+}));
+
+app.get("/downloads", (req, res)=>{
+    console.log(req.ip);
+    res.sendFile(path.join(__dirname, "front.html"));
+});
+
+app.listen(3000, ()=>{
+    console.log("server started");
+});
